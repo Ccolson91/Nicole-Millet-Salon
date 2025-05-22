c0a83a1fdfa9cbf664a95ad6365c0eb41515ec15
Christian Colson <christiancolson91@gmail.com>
2025-05-21 20:13:22 -0500

    thank you page modified

--- src/routes/thank-you/+page.svelte
+++ src/routes/thank-you/+page.svelte
@@ -1,18 +1,7 @@
 
 <script>
-    import {giftCardSubmitted, paymentSubmitted} from '$lib/stores/formStore';
-    import {goto} from '$app/navigation';
-    import {onMount} from 'svelte';
   import { ThankYou } from '$components';
 
-  // Check if user completed the flow
-  /*
-  onMount(() => {
-    if (!$giftCardSubmitted || !$paymentSubmitted) {
-      goto('/');
-    }
-  });
-  */
 </script>
 
 <svelte:head>
